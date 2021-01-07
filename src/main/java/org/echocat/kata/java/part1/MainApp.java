3f47b61c0d9615e877df356394c23e80e30d382a
Viktor Seifert <viktor@viktor-s.net>
2021-01-07 16:34:46 +0100

    Migrated app to spring-boot

--- src/main/java/org/echocat/kata/java/part1/MainApp.java
+++ src/main/java/org/echocat/kata/java/part1/MainApp.java
@@ -1,13 +1,21 @@
 package org.echocat.kata.java.part1;
 
-public class MainApp {
+import org.slf4j.*;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class MainApp implements CommandLineRunner {
+    private static final Logger LOG = LoggerFactory.getLogger(MainApp.class);
 
     public static void main(String[] args) {
-        System.out.println(getHelloWorldText());
+        SpringApplication.run(MainApp.class, args);
     }
 
-    protected static String getHelloWorldText() {
-        return "Hello world!";
+    @Override
+    public void run(String... args) {
+        LOG.info("Application started with " + Arrays.toString(args));
     }
-
 }
